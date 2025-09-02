ca2da9ce56d1418bf92c8f1fc6c406bd80090780
marcel <marcel.zapf@outlook.com>
2025-09-02 16:12:29 +0200

    update

--- terraform/talos/kubeconfig.tf
+++ terraform/talos/kubeconfig.tf
@@ -3,5 +3,5 @@ resource "talos_cluster_kubeconfig" "this" {
     talos_machine_bootstrap.this
   ]
   client_configuration = talos_machine_secrets.this.client_configuration
-  node                 = var.controlplanes[0].ip
+  node                 = var.endpoint_vip
 }
